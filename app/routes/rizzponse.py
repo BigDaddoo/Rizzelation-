65004c74b1308d847e96b52cbd28ce5ebe2ecda1
BigDaddoo <ahmed.a.alowmari@gmail.com>
2023-03-22 14:54:59 -0700

    commit once more

--- app/routes/rizzponse.py
+++ app/routes/rizzponse.py
@@ -7,3 +7,24 @@ from app.classes.data import Blog, Comment
 from app.classes.forms import BlogForm, CommentForm
 from flask_login import login_required
 import datetime as dt
+
+@app.route('/rizzponse/new/<rizzponseID>', methods=['GET', 'POST'])
+@login_required
+def rizzponseNew(rizzponseID):
+    rizzponse = Rizzponse.objects.get(id=rizzponseID)
+    form = RizzponseForm()
+    if form.validate_on_submit():
+        newRizzponse = Rizzponse(
+            author = current_user.id,
+            rizzponse = rizzponseID, 
+            commentary = form.content.data 
+            #! not sure if we should add more things but ehh
+        )
+        newRizzponse.save()
+        return redirect(url_for('rizzponse',rizzponseID=blogID))
+    return render_template('rizzponseform.html',form=form,rizzponse=rizzponse) #! this is weird, might work
+#   return render_template('rizzponseform.html',form=form,blog=blog) 
+
+
+
+
